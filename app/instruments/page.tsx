a2d5472e91ff42ca2a7621e66a3987b9d83a59d7
omegamaster86 <masya868686@gmail.com>
2025-06-28 17:09:33 +0900

    instrumentsの取得時にエラーハンドリングを削除し、コードをさらに簡素化

--- app/instruments/page.tsx
+++ app/instruments/page.tsx
@@ -2,7 +2,7 @@ import { createClient } from '@/lib/supabase/server';
 
 export default async function Instruments() {
   const supabase = await createClient();
-  const { data: instruments, error } = await supabase.from("instruments").select();
+  const { data: instruments } = await supabase.from("instruments").select();
 
   return <pre>{JSON.stringify(instruments, null, 2)}</pre>
 }
