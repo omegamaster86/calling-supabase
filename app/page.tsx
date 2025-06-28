49efefeb9b723a92fee0833e18adb4d960a3d738
omegamaster86 <masya868686@gmail.com>
2025-06-28 20:16:41 +0900

    ページのナビゲーションを更新し、スタイルを改善。新しいタイトルを追加し、環境変数の警告を表示する条件を維持。

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,10 @@ export default async function Home() {
 
   return (
     <main className="min-h-screen flex flex-col items-center">
-      <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-5xl flex justify-end items-center p-3 px-5 text-sm">
+      <div className=" w-full flex flex-col items-center">
+        <nav className="w-full border-b border-b-foreground/10">
+          <div className="bg-sky-500 text-white p-3 text-sm flex justify-between items-center">
+            <p className="font-bold">calling</p>
             {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
           </div>
         </nav>
