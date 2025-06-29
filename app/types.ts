4480173611f94b47e9892907afa0ac6e7e38890a
omegamaster86 <masya868686@gmail.com>
2025-06-29 20:37:58 +0900

    仮の型を設定

---
+++ app/types.ts
@@ -0,0 +1,18 @@
+export interface CallingResultItem {
+  id: number;
+  name: string;
+}
+
+export interface Company {
+  id: number;
+  location: string;
+  calling_result: string;
+  sales_person: string;
+  next_call_date: string;
+  company_name: string;
+  phone_number: string;
+  industry: string;
+  name: string;
+  department: string;
+  notes: string;
+} 
