45d15eca252c998ae362a471a4f78220732ad286
omegamaster86 <masya868686@gmail.com>
2025-07-10 19:41:33 +0900

    AttackLogコンポーネントにヘッダーを追加し、スタイルを適用しました。

--- app/attacklog/page.tsx
+++ app/attacklog/page.tsx
@@ -1,7 +1,10 @@
+import { Header } from "./header";
 
 const AttackLog = () => {
   return (
-    <div>AttackLog</div>
+    <div className="max-w-screen-lg px-16 py-8 mx-auto">
+      <Header />
+    </div>
   )
 }
 
