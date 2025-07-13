b039adabff621b295564c63a42e56bdcb156ae46
omegamaster86 <masya868686@gmail.com>
2025-07-13 13:02:52 +0900

    新しいインターフェースCompanyInfoとAttackLogを追加し、法人情報と攻撃ログの構造を定義しました。

--- app/types/index.tsx
+++ app/types/index.tsx
@@ -2,3 +2,26 @@ export interface CallingResultItem {
   id: number;
   name: string;
 }
+
+export interface CompanyInfo {
+	company_id: number;
+	company_name: string;
+	address: string;
+	company_website: string;
+	registered_phone_number: string;
+	department_name: string;
+	key_person_name: string;
+	key_person_position: string;
+	key_person_phone_number: string;
+	key_person_email: string;
+	key_person_note: string;
+}
+
+export interface AttackLog {
+	id: number;
+	company_id: number;
+	person_id: number;
+	attack_date: string;
+	attack_result: string;
+	attack_note: string;
+}
