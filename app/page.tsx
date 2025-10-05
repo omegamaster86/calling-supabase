b8ef87914a955578f8ed6ce5c53bc938e4abcf28
omegamaster86 <masya868686@gmail.com>
2025-10-05 21:50:12 +0900

    ページのナビゲーションバーの背景色を青からグラデーションに変更し、視覚的な魅力を向上させました。

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
 		<main className="min-h-screen flex flex-col items-center">
 			<div className="w-full flex flex-col items-center">
 				<nav className="w-full">
-					<div className="bg-blue-500 text-white p-3 px-16 text-sm flex justify-between items-center">
+					<div className="bg-gradient-to-r from-[#05B1FA] to-[#64D2FE] text-white p-3 px-16 text-sm flex justify-between items-center">
 						<p className="font-bold">calling</p>
 						{!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
 					</div>
